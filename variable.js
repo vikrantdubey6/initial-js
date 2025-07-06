2fb64d3d6b8e739bfb1e638f5928893f02b18a61
vikrant dubey <Vikrant2003aug06@gmail.com>
2025-07-06 21:33:36 +0530

    Update variable.js

--- variable.js
+++ variable.js
@@ -8,3 +8,5 @@ accountcity = 'Mumbai'
 accountPassword = 'jaiho@123'
 
 console.table([accountId, accountEmail, accountPassword, accountcity]);
+
+// var is not preferred in modern JavaScript, use let or const instead because of issues in block scope and fuctional scope. lorem is not hoisted in the same way as var, and it can lead to unexpected behavior in loops and conditionals.
