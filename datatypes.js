35ba426e7f67a0531a28610e327713bb03ce0c97
vikrant dubey <Vikrant2003aug06@gmail.com>
2025-07-07 02:35:15 +0530

    Add datatypes demo and update variable notes
    Added a new file datatypes.js demonstrating type conversion and type checking in JavaScript. Updated variable.js with additional comments explaining typeof behavior for null, undefined, and NaN, as well as a note on data types.

---
+++ datatypes.js
@@ -0,0 +1,31 @@
+// let score = 54;
+
+//     console.log(typeof score); 
+// console.log(typeof(score)); 
+
+// let trail =Number(score);
+// console.log(typeof trail); 
+// console.log(trail); 
+
+// conversion keyword are Number(), String(), Boolean() and BigInt()
+
+// let isLoggedIn = 0
+
+// let booleanisloggedIn = Boolean(isLoggedIn);
+
+// console.log(booleanisloggedIn)
+
+// console.log(typeof booleanisloggedIn);
+
+
+let x = 10;
+let converter = String(x);
+
+console.log(typeof converter); 
+
+console.log(converter); 
+
+console.log(x); 
+
+console.log(typeof x); 
+
--- variable.js
+++ variable.js
@@ -10,3 +10,11 @@ accountPassword = 'jaiho@123'
 console.table([accountId, accountEmail, accountPassword, accountcity]);
 
 // var is not preferred in modern JavaScript, use let or const instead because of issues in block scope and fuctional scope. lorem is not hoisted in the same way as var, and it can lead to unexpected behavior in loops and conditionals.
+
+
+
+// typeof null returns "object"
+//  whereas undefined returns "undefined".
+//  NaN means not as number, it is being returned when a valuse is not a number.
+
+// DATA TYPES
