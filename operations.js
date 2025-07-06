a63d8ba28f4db656150ce35577288b95aea4ddc4
vikrant dubey <Vikrant2003aug06@gmail.com>
2025-07-07 03:51:52 +0530

    Update operations.js

--- operations.js
+++ operations.js
@@ -1,1 +1,54 @@
 //BASIC OPERATIONS
+// let value = 34
+// let NegValue = -value
+// console.log(NegValue)
+// console.log(value + NegValue)
+
+/* operations
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3); 
+console.log(2/2);
+console.log(10%3);
+*/
+
+
+// let str1 = "Hello";
+// let str2 = " World!";
+// let str3 = str1+str2;
+// console.log(str1+ str2);
+// console.log(str3);
+
+
+
+/*
+console.log(1+2);
+
+console.log(1+"2");
+
+console.log("1"+2);
+
+console.log("1"+"2");
+
+console.log(1+2+"3");
+
+console.log("1+2 "+3);
+
+console.log(1+"2"+"3");
+
+console.log("1"+2+3);
+
+console.log("1"+2+3+"4");
+
+ Output of above code will be:
+ 3
+ 12
+ 12
+ 12
+ 33
+ 1+2 3
+ 123
+ 123
+ 1234
+*/
