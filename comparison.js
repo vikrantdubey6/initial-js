c9e4c2bf24c5b1e157eb29eb7b167c7c33a27725
vikrant dubey <Vikrant2003aug06@gmail.com>
2025-07-07 04:10:21 +0530

    Create comparison.js

---
+++ comparison.js
@@ -0,0 +1,21 @@
+/*console.log(3>1);
+console.log(3==1);
+console.log(3>=1);
+console.log(3!=1);
+
+
+
+console.log("2">1);
+console.log("03">2);
+console.log("2"==2);
+console.log("2"===2);
+console.log("2"!=2);
+
+*/
+
+console.log(null == undefined);
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
