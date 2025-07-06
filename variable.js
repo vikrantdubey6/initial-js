e4756956de0b05bbb924751ea61c365a0375da2f
vikrant dubey <Vikrant2003aug06@gmail.com>
2025-07-06 19:08:15 +0530

    Create variable.js

---
+++ variable.js
@@ -0,0 +1,10 @@
+const accountId = 4322
+let accountEmail = 'vikrant2003aug06@gmail.com'
+var accountPassword = 'vik@123'
+accountcity = 'Delhi'
+
+accountEmail = 'jaiho@gmail.com'
+accountcity = 'Mumbai'
+accountPassword = 'jaiho@123'
+
+console.table([accountId, accountEmail, accountPassword, accountcity]);
