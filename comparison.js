8ddca8efa620a89b4802778de07eba2c8c9ba197
vikrant dubey <Vikrant2003aug06@gmail.com>
2025-07-07 04:14:50 +0530

    Update comparison.js

--- comparison.js
+++ comparison.js
@@ -3,7 +3,7 @@ console.log(3==1);
 console.log(3>=1);
 console.log(3!=1);
 
-
+(=== is strict check, it checks both value and data type)
 
 console.log("2">1);
 console.log("03">2);
@@ -11,11 +11,23 @@ console.log("2"==2);
 console.log("2"===2);
 console.log("2"!=2);
 
-*/
+
 
 console.log(null == undefined);
 console.log(null > 0);
 console.log(null == 0);
 console.log(null >= 0);
+console.log(null < 0);
+console.log(null === undefined);
+console.log(null > undefined);
+console.log(null < undefined);
+*/
+
+
+console.log(undefined == null);
+console.log(undefined > 0); 
+console.log(undefined < 0); 
+console.log(undefined == 0);
+console.log(undefined >= 0);
 
 
