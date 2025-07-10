bae5d9bbd3984df92380aa823d6ef9bf4ed3535b
vikrant dubey <Vikrant2003aug06@gmail.com>
2025-07-11 04:00:35 +0530

    Add number_and_maths.js with initial implementation

---
+++ number_and_maths.js
@@ -0,0 +1,43 @@
+// File: number_and_maths.js
+// --- maths_and_numbers
+const score = 400
+// console.log(score); 
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance);
+
+// console.log(balance.toFixed(2));
+
+const num1 = 34.142
+
+// console.log(num1.toPrecision(3)); 
+
+// toPrecision is used to format a number to a specified precision (total number of significant digits).
+
+const num2 = 10000000
+// console.log(num2.toLocaleString());
+
+// +++++++++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++
+
+console.log(Math); 
+// console.log(Math.PI);
+// console.log(Math.abs(-5)); // absolute value
+// console.log(Math.ceil(4.2)); // rounds up to the nearest integer
+// console.log(Math.floor(4.8)); // rounds down to the nearest integer  
+// console.log(Math.round(4.5)); // rounds to the nearest integer
+
+
+// console.log(Math.max(1, 2, 3, 4, 5)); // returns the largest number
+// console.log(Math.min(1, 2, 3, 4, 5)); // returns the smallest number
+// console.log(Math.ceil(8.1));
+// console.log(Math.floor(8.9));
+// console.log(Math.round(8.6));
+
+
+console.log(Math.random()); // generates a random number between 0 and 1
+console.log(Math.random() * 10); // generates a random number between 0 and 10
+console.log((Math.random() * 10 ) + 1); // generates a random number between 1 and 10
+
