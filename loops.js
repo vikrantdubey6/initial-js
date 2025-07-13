ada3177e531b105a115a7675069ba7c78aa90adb
vikrant dubey <Vikrant2003aug06@gmail.com>
2025-07-13 20:13:17 +0530

    Update loops.js

--- loops.js
+++ loops.js
@@ -1,3 +1,36 @@
- //for
+// loops are also known as iterators
+//for
+for (let index = 1; index <= 10; index++)
+     {
+    const element = index;
 
- for
+//     console.log(element);
+}
+
+for (let i = 1; i <= 10; i++) {
+    // const element = i;
+    // console.log(`this is outer loop  ${i}`);
+    
+    for (let j = 1; j <=10; j++) {
+        // const element = j;
+    // console.log(`this is inner loop ${j}`);
+    // console.log(`${i} x ${j} = ${i*j}`); //table till 10
+    }
+}
+
+let Heros = ["flash", "batman", "superman", "shaktiman"]
+
+// console.log(Heros.length);
+for (let index = 0; index < Heros.length; index++) {
+    const element = Heros[index];
+    // console.log(element);  
+}
+
+// break and continue
+
+for (let index = 1; index <= 20; index++) {
+   if (index<=15) {
+    break;
+       console.log(index)
+   }
+}
