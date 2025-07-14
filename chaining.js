75ee3249262055fb13e785564e94c9a953c4547e
vikrant dubey <Vikrant2003aug06@gmail.com>
2025-07-15 02:32:35 +0530

    Update chaining.js

--- chaining.js
+++ chaining.js
@@ -7,12 +7,39 @@
 //                      console.log(newnum)
 
      //  +++++++++++ reduce() ++++++++++++++++++
-     
-     const mynums = [1, 2, 3]
-     const myTotal = mynums.reduce(function (acc, currval){
-        console.log(`acc: ${acc} and currval: ${currval}`)
-        return acc + currval
-     }, 0)
 
-     console.log(myTotal);
+     const mynums = [1, 2, 3]
+    //  const myTotal = mynums.reduce(function (acc, currval){
+    //     console.log(`acc: ${acc} and currval: ${currval}`)
+    //     return acc + currval
+    //  }, 0)
+
+    
+    // const myTotal = mynums.reduce((acc, currval) =>{acc + currval
+    //     return acc + currval
+    // }, 0)
+    // console.log(myTotal);
      
+    const shoppingcart = [
+        {
+            itemName : "Js course",
+            price : 2999
+        },
+        {
+            itemName : "python course",
+            price : 1999
+        },
+        { 
+            itemName : "App Dev course",
+            price : 999
+        },
+        {
+            itemName : "C++ course",
+            price : 3999
+        }
+
+    ]
+   const PTP =  shoppingcart.reduce((acc, item) => acc + item.price, 0  )
+
+    console.log(PTP);
+    
